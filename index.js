1296a4d5025657bfc5b536d697c3f05ffa3ede90
John S <diaz.john312@gmail.com>
2018-09-02 22:25:52 -0400

    all get requests to server send build when in production

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ require('./services/activeRoom')(io);
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.resolve(__dirname, 'client', 'build')))
 
-  app.get('/', (req, res) => {
+  app.get('*', (req, res) => {
     res.sendFile(__dirname + '/build/index.html');
   })
 };
