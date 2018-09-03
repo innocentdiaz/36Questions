ac20db8b46204ab1b1918745535c6d93a0de9a09
John S <diaz.john312@gmail.com>
2018-09-02 22:31:26 -0400

    Fixed wrong path to build/index.html

--- index.js
+++ index.js
@@ -29,8 +29,8 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.resolve(__dirname, 'client', 'build')))
 
   app.get('*', (req, res) => {
-    res.sendFile(__dirname + '/build/index.html');
-  })
+    res.sendFile(__dirname + '/client/build/index.html');
+  });
 };
 
 const PORT = process.env.PORT || 5000;
