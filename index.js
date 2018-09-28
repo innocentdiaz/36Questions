e13a37654abc469294b311609c67efbb1d893b33
John S <diaz.john312@gmail.com>
2018-09-27 20:16:32 -0400

    Refactored index.js

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const http = require('http');
 const express = require('express');
-const app = express();
 const mongoose = require('mongoose');
 const cors = require('cors');
-const path = require('path')
+const path = require('path');
 
-let { DBHost } = process.env;
+const app = express();
+
+const { DBHost } = process.env;
 
 if (!DBHost) {
-  throw "MISSING ENVIORMENTAL VARIABLE"
+  throw { error: 'MISSING ENVIORMENTAL VARIABLE' };
 }
 
 mongoose.connect(process.env.DBHost, { useNewUrlParser: true });
@@ -17,7 +18,7 @@ app.use(cors());
 
 app.use(express.json());
 
-var server = http.createServer(app);
+const server = http.createServer(app);
 const io = require('socket.io')(server);
 
 require('./lib/routes/auth')(app);
@@ -27,13 +28,15 @@ require('./lib/routes/matching')(io.of('/api/matching'));
 require('./lib/routes/rooms')(io.of('/api/rooms'));
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.resolve(__dirname, 'client', 'build')))
+  app.use(express.static(path.resolve(__dirname, 'client', 'build')));
 
   app.get('*', (req, res) => {
-    res.sendFile(__dirname + '/client/build/index.html');
+    res.sendFile(path.join(__dirname, '/client/build/index.html'));
   });
-};
+}
 
 const PORT = process.env.PORT || 5000;
 
-server.listen(PORT, () => console.log('Listening at port ', PORT))
+server.listen(PORT, () => {
+  console.log('Listening at port ', PORT);
+});
